2f22ea7179c1621a2ad462da51acf72bc44a4d6a
EliteCoder21 <emailaaryan@protonmail.com>
2025-11-21 15:37:04 -0800

    Added some documentation to esp32 master code

--- ESP32/esp32_master/esp32_master.ino
+++ ESP32/esp32_master/esp32_master.ino
@@ -1,6 +1,15 @@
+
+// Define Pin constants
+#define TRIG_PIN 5
+#define ECHO_PIN 18
+
+// Set up serial connection to Arduino Mega (motor slave)
 HardwareSerial SerialMega(2); // UART2, GPIO16=RX, GPIO17=TX (adjust for your board)
 
+// Setup function
 void setup() {
+
+  // 
   Serial.begin(9600);
   SerialMega.begin(9600, SERIAL_8N1, 16, 2); // RX, TX
 }
